6b8bb43052269bd358ecd83a0877c0cf9c3b2a80
adahal1 <adahal1@mail.csuchico.edu>
2016-10-31 16:14:39 -0700

    update

--- sales.js
+++ sales.js
@@ -4,7 +4,10 @@ function addItem()
 {
   var newItem;
   newItem = document.getElementById("price").value;
+  if(isNaN(newItem)){
   //IF newItem is not a number
+  window.alert("Enter price as a number: ");
+  }
   // THEN show an alert: "Enter price as a number"
   //OTHERWISE,
   // update newItem to its value cast as a number
