94510a419182a3d1efa2451e4333606d2323ee32
adahal1 <adahal1@mail.csuchico.edu>
2016-11-02 17:00:55 -0700

    x19: final push

--- sales.js
+++ sales.js
@@ -18,20 +18,18 @@ function addItem()
   }
 }
 
-//takes a number and gives a string with the number displayed as USD currency
 function asCurrency(val)
 {
   return "$" + val.toFixed(2);
 }
 
-//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+
 function getCookie(cname){
     var name = cname + "=";
     var ca = document.cookie.split(';');
